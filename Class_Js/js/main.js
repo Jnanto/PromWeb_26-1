9e836ce4e1e080bd5efd9bd069e6ebeac81a8356
Jnanto <gatros500@gmail.com>
2026-03-18 09:42:00 -0500

    Clase de animaciones y JavaScript

---
+++ Class_Js/js/main.js
@@ -0,0 +1,12 @@
+let x = "1",y = 1,z = "uno", m = ["asd", 3, " was"], n = true; 
+console.log(x + " Es un " + typeof(x))
+console.log(y + " Es un " + typeof(y))
+console.log(z  + " Es un " + typeof(z))
+console.log(n  + " Es un " + typeof(n))
+console.log(m  + " Es un " + typeof(m))
+let a = 6, b =4; 
+let resultado = ~~(10 / 3);
+console.log (resultado);
+let name = prompt("Nombre?");
+alert("hola " + name);
+console.log("Esto es para equipo dev :)");
