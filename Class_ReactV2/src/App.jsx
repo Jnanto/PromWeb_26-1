080044e95a996ed5793b9d90bf5c306538606668
Jnanto <gatros500@gmail.com>
2026-04-09 11:27:33 -0500

    Ultimos cambios en entregable

--- Class_ReactV2/src/App.jsx
+++ Class_ReactV2/src/App.jsx
@@ -28,22 +28,18 @@ function App() {
         <button className="btn btn-util">+/-</button>
         <button className="btn btn-util">%</button>
         <button className="btn btn-op">÷</button>
-
         <button className="btn btn-num" onClick={() => inputDigit(7)}>7</button>
         <button className="btn btn-num" onClick={() => inputDigit(8)}>8</button>
         <button className="btn btn-num" onClick={() => inputDigit(9)}>9</button>
         <button className="btn btn-op">×</button>
-
         <button className="btn btn-num" onClick={() => inputDigit(4)}>4</button>
         <button className="btn btn-num" onClick={() => inputDigit(5)}>5</button>
         <button className="btn btn-num" onClick={() => inputDigit(6)}>6</button>
         <button className="btn btn-op">-</button>
-
         <button className="btn btn-num" onClick={() => inputDigit(1)}>1</button>
         <button className="btn btn-num" onClick={() => inputDigit(2)}>2</button>
         <button className="btn btn-num" onClick={() => inputDigit(3)}>3</button>
         <button className="btn btn-op">+</button>
-
         <button className="btn btn-num btn-zero" onClick={() => inputDigit(0)}>0</button>
         <button className="btn btn-num" onClick={inputDot}>.</button>
         <button className="btn btn-op">=</button>
